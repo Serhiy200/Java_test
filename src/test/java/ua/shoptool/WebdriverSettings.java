e6f20d29b6ca00795407276633a48abefc2335eb
Serhiy20 <you@example.com>
2021-02-08 20:26:36 +0200

    Test task

---
+++ src/test/java/ua/shoptool/WebdriverSettings.java
@@ -0,0 +1,38 @@
+package ua.shoptool;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Random;
+
+public class WebdriverSettings {
+    public WebDriver driver;
+
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+
+        System.out.println("Test start");
+    }
+
+    @After
+    public void close(){
+        System.out.println("Test close");
+        driver.quit();
+
+    }
+
+    public static int count(int n) {
+        Random rnd = new Random();
+        return rnd.nextInt(n);
+    }
+
+
+}
